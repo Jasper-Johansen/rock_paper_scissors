50f6c2298fd1a67916284733efadd41ed108e3d4
Anoop A S <anoopanoop2001@gmail.com>
2025-03-15 20:45:52 +0530

    Define playRound

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function getComputerChoice(){
     let number = Math.floor(Math.random()*3)+1;
+    let computerChoice;
     if (number===1){
         computerChoice = "rock"
 
@@ -13,20 +14,40 @@ function getComputerChoice(){
         computerChoice = "scissors";
     }
 
-    return `Computer chose ${computerChoice}`;
+    return computerChoice;
 }
 
 function getHumanChoice(){
-    let humanChoice = prompt("Enter your choice: Rock, Paper or Sciccors: ");
+    let humanChoice = prompt("Enter your choice: Rock, Paper or Sciccors: ").toLowerCase();
     return humanChoice;
 }
 
-var humanScore=0;
-var computerScore=0;
+let humanScore=0;
+let computerScore=0;
 
 function playRound(humanChoice,computerChoice){
-    humanChoice = humanChoice.tolowerCase();
+
+    if (humanChoice===computerChoice){
+        return "It's a tie!";
+    }
+    else{
+        if (humanChoice==="rock" && computerChoice==="scissors" || humanChoice==="scissors" && computerChoice==="paper" || humanChoice==="paper" && computerChoice==="rock"){
+            humanScore++;
+            return `You win! ${humanChoice} beats ${computerChoice}`;
+        }
+        else{
+            computerScore++;
+            return `You lose! ${computerChoice} beats ${humanChoice}`;
+        }
+    }
 }
 
 const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
+
+let round = playRound(humanSelection,computerSelection);
+
+function playGame(playRound){
+    for (let i = 0; i < 5; i++) {
+        
+}
