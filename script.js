ec58afa3a0dfa4a8411899d0905412262411e5ee
Jasper_Johansen <anoopanoop2001@gmail.com>
2025-03-17 15:11:38 +0530

    add getWinner function, after 5 points, reset score

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let humanScore=0;
 let computerScore=0;
 let round = 1;
 
-function playGame(round){
+function playGame(){
     
     
 
@@ -42,21 +42,40 @@ function playGame(round){
         }
     }
 
-    const computerSelection = getComputerChoice();
     
     let btns = document.querySelectorAll("button");
     let container = document.querySelector(".display");
 
+    function getWinner(){
+        if (humanScore===5 || computerScore===5){
+            if (humanScore>computerScore){
+                computerScore =0;
+                humanScore=0;
+                return "You win the game!";
+            }else{
+                computerScore =0;
+                humanScore=0;
+                return "Computer wins the game!";
+                }
+        
+        }else if(humanScore<5 || computerScore<5){
+            return '';
+        }
+    }
+
     btns.forEach (btn=>{
         btn.addEventListener("click",(e) => {
         const humanSelection = e.target.id;
+        const computerSelection = getComputerChoice();
         container.textContent = `You chose ${humanSelection}
         Computer chose ${computerSelection}`;
         container.textContent += playRound(humanSelection,computerSelection);
-        });
+        container.textContent += `Your score: ${humanScore} Computer Score: ${computerScore}`;
+        container.textContent += getWinner();
+    });
     });
 
     
 }
 
-playGame(round);
+playGame();
